f4a14866d6e38e71e96f5e1a8fbd2b36fd1c57da
Juan Pablo Larghi <140678990+juanlarghi@users.noreply.github.com>
2023-07-30 23:02:41 -0300

    Add files via upload

--- DB & Table Creation.sql
+++ DB & Table Creation.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS insurance.directory (
     agent_id INT,
     customer_id INT,
     phone_number VARCHAR(13) NOT NULL,
-    primary_phone TINYINT NOT NULL DEFAULT 1,
+    primary_phone SMALLINT NOT NULL DEFAULT 1,
     PRIMARY KEY (phone_id),
     INDEX phone(phone_number),
     CONSTRAINT fk_directory_agent FOREIGN KEY (agent_id) REFERENCES agent(agent_id) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS insurance.property (
 	property_type VARCHAR(50) NOT NULL,
     improvements DECIMAL(10,2) DEFAULT 0,
     details VARCHAR(1000) DEFAULT NULL,
-    primary_residence TINYINT(1) NOT NULL DEFAULT 1,
+    primary_residence SMALLINT(1) NOT NULL DEFAULT 1,
 	PRIMARY KEY (property_id),
     CONSTRAINT fk_property_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS insurance.vehicle (
     color VARCHAR(50) NOT NULL,
     doors INT NOT NULL,
     milage INT NOT NULL,
-    customization TINYINT(1) NOT NULL DEFAULT 0,
+    customization SMALLINT(1) NOT NULL DEFAULT 0,
     observations VARCHAR(1000),
     PRIMARY KEY (vehicle_id),
     CONSTRAINT fk_vehicle_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -96,8 +96,6 @@ CREATE TABLE IF NOT EXISTS insurance.coverage (
 
 CREATE TABLE IF NOT EXISTS insurance.policy (
     policy_id INT AUTO_INCREMENT,
-    customer_id INT,
-    agent_id INT,
     property_id INT,
     vehicle_id INT,
     policy_category VARCHAR(100) NOT NULL,
@@ -105,29 +103,27 @@ CREATE TABLE IF NOT EXISTS insurance.policy (
     issued_date DATE NOT NULL,
     expire_date DATE NOT NULL,
     total_amount DECIMAL(10,2),
-    policy_active TINYINT(1) NOT NULL DEFAULT 1,
+    policy_active SMALLINT(1) NOT NULL DEFAULT 1,
     PRIMARY KEY (policy_id),
     INDEX policy_number (policy_id),
-    CONSTRAINT fk_policy_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_policy_agent FOREIGN KEY (agent_id) REFERENCES agent(agent_id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	CONSTRAINT fk_policy_property FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_policy_vehicle FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_policy_coverage FOREIGN KEY (coverage_id) REFERENCES coverage(coverage_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS insurance.accident (
-	report_number INT NOT NULL UNIQUE AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS insurance.incident (
+	report_number INT NOT NULL AUTO_INCREMENT,
     property_id INT,
     vehicle_id INT,
-    accident_type VARCHAR(50) NOT NULL,
-    accident_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    location VARCHAR(100) NOT NULL,
-    accident_record VARCHAR(1000),
+    incident_type VARCHAR(50) NOT NULL,
+    incident_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    incident_location VARCHAR(100) NOT NULL,
+    incident_record VARCHAR(1000),
     damage_estimate DECIMAL(10,2) NOT NULL,
-    at_fault TINYINT(1),
+    at_fault SMALLINT(1),
     PRIMARY KEY (report_number),
-	CONSTRAINT fk_accident_property FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_accident_vehicle FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id) ON DELETE RESTRICT ON UPDATE CASCADE
+	CONSTRAINT fk_incident_property FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_incident_vehicle FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS insurance.payment (
@@ -140,7 +136,7 @@ CREATE TABLE IF NOT EXISTS insurance.payment (
     due_date DATETIME NOT NULL DEFAULT (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 30 DAY)),
 	paid_date DATE,
     payment_method VARCHAR(50) NOT NULL,
-    debitorcredit TINYINT NOT NULL,
+    debitorcredit SMALLINT NOT NULL,
     additional_info VARCHAR(50) DEFAULT NULL,
     PRIMARY KEY (payment_id),
     INDEX invoice_number (payment_id),
@@ -152,9 +148,9 @@ CREATE TABLE IF NOT EXISTS insurance.payment (
 CREATE TABLE IF NOT EXISTS insurance.license (
 	license_id INT AUTO_INCREMENT,
 	customer_id INT,
-    lic_issued_date DATE NOT NULL,
-    lic_expire_date DATE NOT NULL,
-    license_active TINYINT(1) NOT NULL DEFAULT 1,
+    lic_issued_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    lic_expire_date DATE NOT NULL DEFAULT(DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 5 YEAR)),
+    license_active SMALLINT(1) NOT NULL DEFAULT 1,
     PRIMARY KEY (license_id),
 	INDEX driver_license (license_id),
     CONSTRAINT fk_license_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE RESTRICT ON UPDATE CASCADE
